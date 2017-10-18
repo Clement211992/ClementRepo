2742d24b3d14fc0cb75a7fc3557f9101939ee314
Clement211992 <leodinesh21@gmail.com>
2017-10-18 19:13:04 +0530

    updated fb login

--- src/POM/FB_Login.java
+++ src/POM/FB_Login.java
@@ -10,30 +10,32 @@ import org.testng.annotations.Test;
 public class FB_Login {
 	WebDriver driver;
 	FB_Page_Repo FB_Page;
-	
-  @BeforeTest
-  public void setInitials() {
-	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Clement\\Documents\\Java_Eclipse\\chromedriver_win32\\chromedriver.exe");
-	  driver = new ChromeDriver();
-	  
-	  driver.get("https://www.facebook.com/");
-	  
-  }
-	
-  @Test
-  public void LoginMethod(){
-	  FB_Page = new FB_Page_Repo(driver);
-	  FB_Page.LoginFB("Hello", "123");
-	  System.out.println("PAge Opened");
-  }
-  
-  @AfterTest
-  public void close_Browser() {
-	  driver.close();
-	  System.out.println("Page Closed");
-  }
 
-  //public String GetTitle() {
-//	return GetTitle();
-//}
+	@BeforeTest
+	public void setInitials() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Clement\\Documents\\Java_Eclipse\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com/");
+
+	}
+
+	@Test
+	public void LoginMethod() {
+		FB_Page = new FB_Page_Repo(driver);
+		FB_Page.LoginFB("Hello", "123");
+		System.out.println("PAge Opened");
+	}
+
+	@Test
+	public void print() {
+
+	}
+
+	@AfterTest
+	public void close_Browser() {
+		driver.close();
+		System.out.println("Page Closed");
+	}
 }
